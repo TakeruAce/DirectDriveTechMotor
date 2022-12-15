b881e1a77fc00560ebf271182dd29407b0c94e50
TKR <apple.takeru.0309@gmail.com>
2022-12-15 17:40:05 +0900

    avoid angle value jump when communication failed

--- src/main.ino
+++ src/main.ino
@@ -11,10 +11,12 @@ int16_t Speed = 0;   // Speed of motor
 uint8_t Acce = 0;    // Acceleration of motor
 uint8_t Brake_P = 0; // Brake position of motor
 uint8_t IDs[] = {1,3};      // ID of Motor (default:1)
-double angle_offset[] = {-0.536,1.715};
+double angle_offset[] = {-0.536,1.0853};
+double prev_angle[] = {0,0};
 double motor_direction[] = {-1,1};
 
-Receiver Receiv;
+Receiver Receiv[2];
+Receiver prev_Receiv[2];
 // ,TXのピン番号が違うためM5製品とRS485モジュールに対応させてくださいM5Stackのモジュールによって対応するRX
 auto motor_handler = MotorHandler(1, 0); // RX,TX
 const int16_t SPEED_MAX = 40;
@@ -47,7 +49,7 @@ void setup()
   // velocity mode: 0x02
   // position mode: 0x03
   for (int i=0;i<sizeof(IDs)/sizeof(IDs[0]);i++) {
-    motor_handler.Control_Motor(0, IDs[i], Acce, Brake_P, &Receiv); //モータ停止
+    motor_handler.Control_Motor(0, IDs[i], Acce, Brake_P, &Receiv[i]); //モータ停止
     delay(100);
     motor_handler.Set_MotorMode(Mode, IDs[i]);
     delay(100);
@@ -89,30 +91,33 @@ void loop()
 {
   float t = millis();
   for (int i=0;i<sizeof(IDs)/sizeof(IDs[0]);i++) {
-    spin_and_get(-motor_direction[i] * cmd_effort[i] / 0.37 / 8.0 * 32767.0, IDs[i]);
-    msg_joint.position[i] = motor_direction[i] * Receiv.Position / 32767.0 * 2 * PI - angle_offset[i]; // rad
+    spin_and_get(-motor_direction[i] * cmd_effort[i] / 0.37 / 8.0 * 32767.0, IDs[i], Receiv[i], prev_Receiv[i]);
+    msg_joint.position[i] = motor_direction[i] * Receiv[i].Position / 32767.0 * 2 * PI - angle_offset[i]; // rad
+    msg_joint.velocity[i]=  motor_direction[i] * Receiv[i].BSpeed / 60.0 * 2 * PI; // rad/s
+    msg_joint.effort[i] = motor_direction[i] * Receiv[i].ECurru / 32767.0 * 8.0 * 0.37; // Nm
+
     // convert angle to (-pi,pi)
     if (msg_joint.position[i] < -PI) msg_joint.position[i] += 2* PI;
     if (msg_joint.position[i] >  PI) msg_joint.position[i] -= 2* PI;
-
-    msg_joint.velocity[i] = motor_direction[i] * Receiv.BSpeed / 60.0 * 2 * PI; // rad/s
-    msg_joint.effort[i] = motor_direction[i] * Receiv.ECurru / 32767.0 * 8.0 * 0.37; // Nm
+    prev_angle[i] = msg_joint.position[i];
+    prev_Receiv[i] = Receiv[i];
   }
   // delay(10);
   pub_joint.publish(&msg_joint);
   nh.spinOnce();
 
-  while(micros()-prev_micro < 10000);
+  while(micros()-prev_micro < 5000);
   // Serial.println(micros()-prev_micro);
   prev_micro = micros();
 }
 
-void spin_and_get(int16_t _Speed, uint8_t id)
+void spin_and_get(int16_t _Speed, uint8_t id, Receiver& Receiv, Receiver& prev_Receiv)
 {
   if(motor_handler.Control_Motor(_Speed, id, Acce, Brake_P, &Receiv)) {
 
   } else {
-    Serial.println("receive failed.");
+    Serial.print("receive failed.");
+    Receiv = prev_Receiv;
   }
   //温度を取得したい場合はGet_Motor関数を呼び出す
   // Serial.print("ID:");
